3b59d7406495c283c7ba388475bed738b6dc8410
Mark Powell <Mark.Powell@computer.org>
2021-06-04 16:16:12 -0700

    Initial Commit

---
+++ Shared/ResultBuilderRhymesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResultBuilderRhymesApp.swift
+//  Shared
+//
+//  Created by Mark Powell on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ResultBuilderRhymesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
